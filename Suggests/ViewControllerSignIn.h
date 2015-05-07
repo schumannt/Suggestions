a86ee963f11e63f1d0cec96354d483489d8bba54
Group 16 <victoria.sainter@uk.tesco.com>
2015-05-07 13:08:44 +0100

    first commit

---
+++ Suggests/ViewControllerSignIn.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Suggests
+//
+//  Created by group16 on 07/05/2015.
+//  Copyright (c) 2015 Tesco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerSignIn : UIViewController
+
+- IBAction BtnSignIn;
+
+@end
+
---
+++ Suggests/ViewControllerSignIn.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Suggests
+//
+//  Created by group16 on 07/05/2015.
+//  Copyright (c) 2015 Tesco. All rights reserved.
+//
+
+#import "ViewControllerSignIn.h"
+
+@interface ViewControllerSignIn ()
+
+@end
+
+@implementation ViewControllerSignIn
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
