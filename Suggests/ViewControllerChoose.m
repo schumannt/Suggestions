1ef702ccd5bd842032c898e1573f7c1609757bfc
Victoria Sainter <victoriasainter@Mikes-MacBook-Pro.local>
2015-05-07 20:41:07 +0100

    kris' crap code

--- Suggests/ViewControllerChoose.m
+++ Suggests/ViewControllerChoose.m
@@ -10,10 +10,6 @@
 
 @interface ViewControllerChoose ()
 
-{
-    NSArray *_pickerData;
-}
-
 @end
 
 @implementation ViewControllerChoose
@@ -24,57 +20,103 @@
     
     // Initialize Data
     
-#pragma mark -
-#pragma mark Picker code
+    self.marMeal = [[NSMutableArray alloc] init];
+    [self.marMeal addObject:@"Breakfast"];
+    [self.marMeal addObject:@"Lunch"];
+    [self.marMeal addObject:@"Snacks"];
+    [self.marMeal addObject:@"Dinner"];
     
+    self.marType = [[NSMutableArray alloc] init];
+    [self.marType addObject:@"Healthy"];
+    [self.marType addObject:@"Naughty"];
     
-    // Initialize Data
-    _pickerData = @[ @[@"Breakfast", @"Lunch", @"Snacks", @"Dinner"],
-                     @[@"Healthy", @"Naughty", @"Healthy", @"Naughty", ],
-                     //@[@"!", @"#", @"$", @"%"],
-                     @[@"£", @"££", @"£££", @"s"] ];
-    
-    // Connect data
-    self.picker.dataSource = self;
-    self.picker.delegate = self;
+    self.marCost = [[NSMutableArray alloc] init];
+    [self.marCost addObject:@"£"];
+    [self.marCost addObject:@"££"];
+    [self.marCost addObject:@"£££"];
 }
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+
+//returns the number of 'columns' to display.
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    //Three columns
+    return 3;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    //set number of rows
+    if(component== 0)
+    {
+        return [self.marMeal count];
+    }
+    if(component== 1)
+    {
+        return [self.marType count];
+    }
+    else
+    {
+        return [self.marCost count];
+    }
+}
+
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
+{
+    NSLog(@"Selected Row %ld", (long)row);
+    
+    NSInteger firstComponentRow = [self.picker selectedRowInComponent:0];
+    NSInteger secondComponentRow = [self.picker selectedRowInComponent:1];
+    NSInteger thirdComponentRow = [self.picker selectedRowInComponent:2];
+    
+    switch(firstComponentRow)
+    {
+        case 0:
+            self.lblMealChoice.text = @"Breakfast";
+            break;
+        case 1:
+            self.lblMealChoice.text = @"Lunch";
+            break;
+        case 2:
+            self.lblMealChoice.text = @"Snacks";
+            break;
+        case 3:
+            self.lblMealChoice.text = @"Dinner";
+            break;
+    }
+    
+    switch(secondComponentRow) {
+        case 0:
+            self.lblTypeChoice.text = @"Healthy";
+            break;
+        case 1:
+            self.lblTypeChoice.text = @"Naughty";
+            break;
+    }
+    
+    switch(thirdComponentRow){
+        case 0:
+            self.lblCostChoice.text = @"£";
+            break;
+        case 1:
+            self.lblCostChoice.text = @"££";
+            break;
+        case 2:
+            self.lblCostChoice.text = @"£££";
+            break;
+    }
+}
+
 - (IBAction) BtnSelect {
     
 }
 
-#pragma mark -
-#pragma mark Picker code
-
-// The number of columns of data
-- (int)numberOfComponentsInPickerView:(UIPickerView *)pickerView
-{
-    return 3;
-}
-
-// The number of rows of data
-- (int)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
-{
-    return _pickerData.count;
-}
-
-// The data to return for the row and component (column) that's being passed in
-- (NSString*)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
-{
-    return _pickerData[component][row];
-}
-
-
-// Catpure the picker view selection
-- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
-{
-    // This method is triggered whenever the user makes a change to the picker selection.
-    // The parameter named row and component represents what was selected.
-}
 
 @end
